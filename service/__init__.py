a1dfa050969c72c447a57d5156dcc019614daffc
mideployco <omotoshoayomide2020@gmail.com>
2026-08-13 17:48:42 -0400

    Fix trailing whitespace and newline formatting

--- service/__init__.py
+++ service/__init__.py
@@ -6,11 +6,13 @@ from flask import Flask
 app = Flask(__name__)
 
 # This must be imported after the Flask app is created
-from service import routes               # pylint: disable=wrong-import-position,cyclic-import
-from service.common import log_handlers  # pylint: disable=wrong-import-position
+from service import routes  # noqa: E402
+from service.common import log_handlers  # noqa: E402
 
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(
+    "  S E R V I C E   R U N N I N G  ".center(70, "*")
+)
 app.logger.info(70 * "*")
--- service/common/log_handlers.py
+++ service/common/log_handlers.py
@@ -29,10 +29,14 @@ def init_logging(app, logger_name: str):
     gunicorn_logger = logging.getLogger(logger_name)
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+
     # Make all log formats consistent
     formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", "%Y-%m-%d %H:%M:%S %z"
+        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s",
+        "%Y-%m-%d %H:%M:%S %z",
     )
+
     for handler in app.logger.handlers:
         handler.setFormatter(formatter)
+
     app.logger.info("Logging handler established")
